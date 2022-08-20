2af4600cf4c71788fa83e6487a60be479d45f80c
johndward01 <johndward01@gmail.com>
2022-08-19 23:12:31 -0500

    Add project files.

---
+++ SendGrid_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using SendGrid_Demo;
+
+SendGridExample.Execute().Wait();
+    
+
+   
---
+++ SendGrid_Demo/SendGridExample.cs
@@ -0,0 +1,21 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SendGrid_Demo;
+
+internal class SendGridExample
+{
+    public static async Task Execute()
+    {
+        Console.Write("Enter your Api Key: ");
+        var apiKey = Console.ReadLine();
+        var client = new SendGridClient(apiKey);
+        var from = new EmailAddress("johndward01@gmail.com", "The sender");
+        var subject = "Sending with SendGrid is Fun";
+        var to = new EmailAddress("jward@truecoders.io", "The receiver");
+        var plainTextContent = "and easy to do anywhere, even with C#";
+        var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response = await client.SendEmailAsync(msg);
+    }
+}
